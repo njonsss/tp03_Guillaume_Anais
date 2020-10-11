fad3e0fabf784d30a2565ff40b71af287c77d8fe
njonsss <senelah960@gmail.com>
2020-10-11 22:49:03 +0200

    4 files modified

--- src/app/produit/produit.component.ts
+++ src/app/produit/produit.component.ts
@@ -5,19 +5,13 @@ import {Produit} from '../models/Produit';
   templateUrl: './produit.component.html',
   styleUrls: ['./produit.component.css']
 })
-export class ProduitComponent implements OnInit {
+
+export class ProduitComponent{
   public model: Produit;
   public listeProduit: Produit[];
 
   constructor() {
-    this.listeProduit = this.storage.get('Catalogue'); //recupération du catalogue courant pour récupérer le produit à afficher
-    let myId = '';
-    this.route.params.subscribe(params => {
-      myId = params['id']; //récupération de l'indice du produit envoyé par l'url
-    });
 
-    this.model = this.listeProduit[myId]; //on définit le modèle avec l'indice du tableau du catalogue
-    this.indexModel = myId;
   }
 
 }
